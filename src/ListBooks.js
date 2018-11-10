62b13ca016078ffe5d57701d1034f226ca4553af
Polly <23667108+PollyannaRibeiro@users.noreply.github.com>
2018-11-10 21:36:00 +0000

    added searchBooks state to ListBooks

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
-import * as BooksAPI from './BooksAPI'
 import * as BooksApp from './App'
+import * as BooksAPI from './BooksAPI'
+
 
 
 class ListBooks extends Component {
@@ -14,57 +15,84 @@ class ListBooks extends Component {
     }
 
     state = {
-        query: ''
+        query: '',
+        searchBooks:[]
     }
-
+    
+    time;
+    
     updateQuery = (query) =>{
-        this.setState({query: query.trim()})
+        this.setState({query: query.trim(), searchBooks:[]})
+        clearInterval(this.time)
+        this.searchingBook(query)
+        
+
     }
 
-    clearQuery = () => {
-        this.setState({query: ''})
+    
+    searchingBook(query){
+        this.time = setTimeout(()=>{
+            BooksAPI.search(this.state.query).then((result) => {
+                this.state.searchBooks = result
+                this.setState(this.state)
+            });
+        }, 500) 
     }
+
+    // keyPress(event){
+    //     if(event.keyCode == 13){
+    //         searchingBook(this.state.query)
+    //     }
+    // }
  
     render(){
-        let showingBooks
-        if(this.state.query){
-            const match = new RegExp(escapeRegExp(this.state.query), 'i')
-            showingBooks = this.props.books.filter((book) => match.test(book.title))
-        } else {
-            showingBooks = this.props.books
-        }
+        // let showingBooks
+        // if(this.state.query){
+        //     const match = new RegExp(escapeRegExp(this.state.query), 'i')
+        //     showingBooks = this.state.searchBooks.filter((book) => match.test(book.title))
+        // } else {
+        //     showingBooks = this.state.searchBooks
+        // }
+        let showingBooks = this.state.searchBooks
 
         return(
-            <div className='search-books-input-wrapper'>
-                <input 
-                    type="text" 
-                    placeholder="Search by title or author" 
-                    value={this.state.query}
-                    onChange={(event) => this.updateQuery(event.target.value)}/>
-
-                <ol className='book-list'>
-                    {showingBooks.map((book)=>(
-                        <li key={book.id} className= 'book-list-item'>
-                            <div className="book">
-                                <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 174, backgroundImage:`url(${book.url})`}}></div>
-                                <div className="book-shelf-changer">
-                                    <select>
-                                    <option value="move" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                    </select>
+            <div>
+                <div className='search-books-bar'>
+                    <Link to='/' className="close-search">Close</Link>
+                    <div className='search-books-input-wrapper'>
+                        <input 
+                        type="text" 
+                        placeholder="Search by title or author" 
+                        value={this.state.query}
+                        onChange={(event) => this.updateQuery(event.target.value)}/>
+                    </div>
+                </div>  
+                <div className="search-books-results">
+                    <ol className='books-grid'>
+                        {showingBooks.map((book)=>(
+                            <li key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                    <div className="book-cover" style={{ width: 128, height: 174, backgroundImage:`url(${book.imageLinks.smallThumbnail})`}}></div>
+                                    <div className="book-shelf-changer">
+                                        <select>
+                                        <option value="move" disabled>Move to...</option>
+                                        <option value="currentlyReading">Currently Reading</option>
+                                        <option value="wantToRead">Want to Read</option>
+                                        <option value="read">Read</option>
+                                        <option value="none">None</option>
+                                        </select>
+                                    </div>
+                                    </div>
+                                    <div className="book-title">{book.title}</div>
+                                    <div className="book-authors">{book.authors}</div>
+                                    <div className="book-category">{book.categories}</div>
                                 </div>
-                                </div>
-                                <div className="book-title">{book.title}</div>
-                                <div className="book-authors">{book.author}</div>
-                            </div>
-                        </li>
-                    ))}
-                </ol>
-            </div>            
+                            </li>
+                        ))}
+                    </ol>
+                </div>   
+            </div>       
         )
         
     }
