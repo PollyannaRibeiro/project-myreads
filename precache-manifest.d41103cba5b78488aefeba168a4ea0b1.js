aac856fd4a4f142bba7116fb0b16005f91a2ebb2
Polly <23667108+PollyannaRibeiro@users.noreply.github.com>
2019-08-04 15:08:47 +0100

    Updates

---
+++ precache-manifest.d41103cba5b78488aefeba168a4ea0b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f4e7584af6865d849c5226ba0e478d",
+    "url": "/project-myreads/index.html"
+  },
+  {
+    "revision": "844f69867872ee21788e",
+    "url": "/project-myreads/static/css/main.0aa8db2f.chunk.css"
+  },
+  {
+    "revision": "6a50433518a457c4a2b3",
+    "url": "/project-myreads/static/js/2.9cfcf9e3.chunk.js"
+  },
+  {
+    "revision": "844f69867872ee21788e",
+    "url": "/project-myreads/static/js/main.fe8205d9.chunk.js"
+  },
+  {
+    "revision": "47b5c7eca53d3ce100f8",
+    "url": "/project-myreads/static/js/runtime~main.25093bc3.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/project-myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/project-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/project-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
